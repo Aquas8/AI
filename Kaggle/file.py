5bb92928f57944597cfc2b73816cb44ae46420b9
Aqua <144764271+Aquas8@users.noreply.github.com>
2025-06-09 14:37:10 +0530

    Update file.py

--- Kaggle/file.py
+++ Kaggle/file.py
@@ -1,1 +1,12 @@
+print("Hello, world!")
+
+print(((1 + 3) * (9 - 2) / 2) ** 2)
+
+
+# Create a variable called test_var and give it a value of 4+5
+test_var = 4 + 5
+
+# Print the value of test_var
+print(test_var)
+
 
